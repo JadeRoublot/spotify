05f82445d694d0dd56242b93d5c39d4e5321b630
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-03-02 14:08:05 +0100

    Little update

---
+++ src/Slices/playlistsSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import React, { useState } from 'react';
+
+export interface Playlist {
+    value: string;
+    label: string;
+}
+
+
+
+export const Add = createSlice({
+    name: 'todolist',
+    initialState: {
+       
+       
+        
+    },
+
+    reducers: {
+
+       
+       }
+
+    },
+);
+
+export const {} = Add.actions;
+
+export default Add.reducer;
