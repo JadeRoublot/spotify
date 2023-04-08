186b93cb7357b1d419b9562c6068c4bf257f434f
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-04-08 18:24:10 +0200

    Update

--- src/Slices/playlistsSlice.ts
+++ src/Slices/playlistsSlice.ts
@@ -18,7 +18,7 @@ export interface Music {
     popularity: number
 }
 
-
+let setColorPlaylist = () => (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
 
 export const playlistsSlice = createSlice({
     name: 'playlist',
