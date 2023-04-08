0a0e4a143c46c064e31c7c309fb608b4bb133146
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-04-08 16:07:59 +0200

    Update SpotifyFooter.tsx

--- src/SpotifyFooter/SpotifyFooter.tsx
+++ src/SpotifyFooter/SpotifyFooter.tsx
@@ -1,16 +1,47 @@
-import { Input, Modal, Table } from 'antd';
+import { Button, Col, Row, Slider} from 'antd';
 import React, { useEffect, useState } from 'react';
 
 import './SpotifyFooterCss.css';
+import { CaretRightOutlined, RetweetOutlined, StepBackwardOutlined, StepForwardOutlined } from '@ant-design/icons';
 
 const SpotifyFooter = () => {
   
+    const [inputValue, setInputValue] = useState(0);
 
+    const onChange = (newValue: number) => {
+      setInputValue(newValue);
+    };
     return (
       
-           <div >
-              <h1>hi</h1>
-            </div>
+        <div >
+            <Row>
+                <Col span={12}>
+
+                    <h1>hi</h1>
+
+                </Col>
+                <Col span={12}>
+               
+                    <StepBackwardOutlined />
+
+                    <Button  shape="circle">
+                        <CaretRightOutlined />
+                    </Button>
+
+                    <StepForwardOutlined />
+                    <RetweetOutlined />
+           
+                    <Slider
+                        min={0}
+                        max={20}
+                        onChange={onChange}
+                        value={typeof inputValue === 'number' ? inputValue : 0}
+                        />
+                </Col>
+            </Row>
+            
+           
+        </div>
       
     );
 };
