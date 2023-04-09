cc5967109bb0ed1f51c48c459c70f5cd78b17f36
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-04-09 17:24:23 +0200

    Update playlistsSlice.ts

--- src/Slices/playlistsSlice.ts
+++ src/Slices/playlistsSlice.ts
@@ -1,22 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 import React, { useState } from 'react';
-
+import data from '../static/data.json';
 
 export interface Playlist {
-    title: string
-    music : Music[]
-    color1 : string
-    color2 : string
+    title: string,
+    music : Music[],
+    color1 : string,
+    color2 : string,
+    
+}
+export interface PlaylistTop extends Playlist {
+    year: number;
 }
 
 export interface Music {
-    title: string
-    artist: string
-    genre: string
-    year: number
-    duration: number
-    popularity: number
+    title: string,
+    artist: string,
+    genre: string,
+    year: number,
+    duration: number,
+    popularity: number,
 }
+let setColorPlaylist = () => (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
 
 const likedSongPlaylist: Playlist = {
     title : "Liked Songs",
@@ -25,22 +30,47 @@ const likedSongPlaylist: Playlist = {
     color2: "#C0ECD7"
 }
 
+const createTop50 = () => {
+    const listTop50 = [];
+    const musics = data;
+
+    for (let i = 2010; i < 2020; i++) {
+        let musicbyYears = musics.filter(musics => musics.year === i)
+        
+        const top50: PlaylistTop = {
+            title: "TOP 50",
+            music: musicbyYears,
+            color1: setColorPlaylist(),
+            color2: setColorPlaylist(),
+            year: i
+        };
+
+        listTop50.push(top50);
+    }
+
+    return listTop50;
+}
+
 export const playlistsSlice = createSlice({
-    name: 'playlist',
+    name: 'spotify',
     initialState: {
-      
-       
+        playlist: [ likedSongPlaylist ,],
+        playlistTop: createTop50(),
+        displayModal: false,
+        music : data,
         
     },
 
     reducers: {
-
+        setPlaylist: (state: {playlist: Playlist[]}, action: { payload: Playlist[] }) => {
+            state.playlist = action.payload;
+        },
        
        }
 
     },
 );
 
-export const {} =  playlistsSlice.actions;
+export const {setPlaylist} =  playlistsSlice.actions;
 
 export default  playlistsSlice.reducer;
