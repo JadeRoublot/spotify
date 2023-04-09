5eeab87eb77a735c69da66c5f34f14a2c4bbea2e
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-04-09 18:06:49 +0200

    Update

--- src/MainPage/CardPlaylist/CardPlaylist.tsx
+++ src/MainPage/CardPlaylist/CardPlaylist.tsx
@@ -3,7 +3,8 @@ import {  Footer } from 'antd/es/layout/layout';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { State } from '../../store';
-
+import { Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 
 const CardPlaylist = () => {
