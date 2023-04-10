0f4ab06287f9e7448fedeb5cc4cb2355a76a439c
TheCrow <77794984+JadeRoublot@users.noreply.github.com>
2023-04-10 23:49:00 +0200

    Update HeaderPlaylist.tsx

--- src/PlaylistPage/Header/HeaderPlaylist.tsx
+++ src/PlaylistPage/Header/HeaderPlaylist.tsx
@@ -4,6 +4,7 @@ import { Header } from 'antd/es/layout/layout';
 
 import './HeaderPlaylistCss.css';
 import { Playlist, PlaylistTop } from '../../Slices/playlistsSlice';
+import { HeartFilled } from '@ant-design/icons';
 
 interface Props {
     other: Playlist | PlaylistTop;
@@ -11,11 +12,16 @@ interface Props {
 
 const HeaderPlaylist = ({ other }: Props) => {
     let title;
+    let display;
 
     if ('year' in other) {
         title = other.title +  ' ' + other.year;
-    } else {
+        display = title;
+    } else if (other.title === 'Liked Songs') {
+        display = <HeartFilled />;
+    }else {
         title = other.title;
+        display = '';
     }
 
     return (
@@ -23,7 +29,7 @@ const HeaderPlaylist = ({ other }: Props) => {
            <Card className={'CardPlaylistHeade'} bordered={false} style= {{background: `linear-gradient( #${other.color1} 0%, #${other.color2} 100%)`}}>
           <Card className={'CardPlaylistCoverMainPage'} bordered={false} style= {{background: `linear-gradient( #${other.color1} 0%, #${other.color2} 100%)`}}>       
           
-          
+            {display}
           </Card>
             <div className={'textHeader'} > 
               <h1>{title}</h1>
